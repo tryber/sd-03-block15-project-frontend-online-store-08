fdbc8894b99d6bd21c86d0462019092ff1256cf7
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-12 18:18:46 -0300

    Modulo api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 export async function getCategories() {
   // implement here
+  const urlCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const data = await fetch(urlCategories);
+  const dataJson = await data.json();
+  return dataJson;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
+  const urlCategories = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const data = await fetch(urlCategories);
+  const dataJson = await data.json();
+  return dataJson.results;
 }
