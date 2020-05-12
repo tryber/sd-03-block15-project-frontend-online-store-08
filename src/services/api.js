a00b213f5d7f839e14f9e10a2a2b423d744deb91
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-12 19:13:21 -0300

    codeclimate

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,14 @@
 export async function getCategories(url) {
   // implement here
   fetch(url)
-  .then(data => data.json())
-  .then((dataJson) => {
-    return dataJson;
-  });
+  .then((data) => data.json())
+  .then((dataJson) => { return dataJson; });
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
   const urlCategories = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   fetch(urlCategories)
-  .then(data => data.json())
-  .then((dataJson) => {
-    return dataJson.results;
-  });
+  .then((data) => data.json())
+  .then((dataJson) => { return dataJson.results;});
 }
