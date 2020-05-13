e586a8c60b79e00a03f23542e36cad00e5359ccb
tales <talestales@gmail.com>
2020-05-13 18:12:57 -0300

    carrinho/4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainScreen from './components/MainScreen';
 import Carrinho from './components/Carrinho';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MainScreen} />
         <Route exact path="/carrinho" component={Carrinho} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import HeaderCarrinho from './HeaderCarrinho';
+import ItemCarrinho from './ItemCarrinho';
 import CarrinhoVazio from './CarrinhoVazio';
 import './Carrinho.css';
 
@@ -14,25 +16,91 @@ class Carrinho extends Component {
     };
     this.testaCarrinhoVazio = this.testaCarrinhoVazio.bind(this);
     this.carregaCarrinhoVazio = this.carregaCarrinhoVazio.bind(this);
+    this.somaTotal = this.somaTotal.bind(this);
+    this.setaValorTotal = this.setaValorTotal.bind(this);
+  }
+
+  componentDidMount() {
+    this.carrinhoMount();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { qtdeItensCarrinho, atualizaSoma } = this.state;
+    if ((qtdeItensCarrinho === 0) && qtdeItensCarrinho !== prevState.qtdeItensCarrinho) {
+      this.carregaCarrinhoVazio();
+    }
+    if (atualizaSoma !== prevState.atualizaSoma
+      || qtdeItensCarrinho !== prevState.qtdeItensCarrinho) {
+      this.setaValorTotal();
+    }
+  }
+
+  setaValorTotal() {
+    const guardar = JSON.parse(localStorage.getItem('Produtos') || '[]');
+    const guardarMap = guardar.map((item) => item.price * item.quant);
+    const guardarReduce = guardarMap.reduce((acc, curr) => acc + curr, 0);
+    localStorage.setItem('SomaTotal', JSON.stringify(guardarReduce));
+    this.setState(() => ({
+      valorTotal: guardarReduce,
+    }));
+  }
+
+  carrinhoMount() {
+    const guardarX = JSON.parse(localStorage.getItem('Produtos') || '[]');
+    if (guardarX.length > 0) {
+      this.setState({
+        qtdeItensCarrinho: guardarX.length,
+        itensCarrinho: guardarX,
+        carrinhoVazio: false,
+      });
+    }
   }
 
   carregaCarrinhoVazio() {
-    this.setState({ carrinhoVazio: true });
+    this.setState({
+      carrinhoVazio: true,
+    });
   }
 
   testaCarrinhoVazio() {
     const { qtdeItensCarrinho } = this.state;
-    this.setState(() => ({ qtdeItensCarrinho: qtdeItensCarrinho - 1 }));
+    this.setState(() => ({
+      qtdeItensCarrinho: qtdeItensCarrinho - 1,
+    }));
+  }
+
+  somaTotal(param1, param2) {
+    this.setState({
+      atualizaSoma: `{${param2} - ${param1}}`,
+    });
   }
 
   render() {
-    const { carrinhoVazio } = this.state;
+    const { itensCarrinho, carrinhoVazio, valorTotal } = this.state;
 
     if (carrinhoVazio) return <CarrinhoVazio />;
 
     return (
       <div className="Carrinho">
         <HeaderCarrinho />
+        {itensCarrinho.map((item) => (
+          <ItemCarrinho
+            key={`${item.id} ${item.quant}`}
+            id={item.id}
+            title={item.title}
+            image={item.thumbnail}
+            price={item.price}
+            quant={item.quant}
+            callbackCarrinhoVazio={this.testaCarrinhoVazio}
+            callbackSomaTotal={this.somaTotal}
+          />
+        ))}
+        <p>
+          <span className="textoCompraTotal">Valor Total da Compra: </span>
+          {new Intl.NumberFormat('pt-BR',
+            { style: 'currency', currency: 'BRL' }).format(valorTotal)}
+        </p>
+        <Link className="botaoCheckout" to="/carrinho/checkout">Finalizar Compra</Link>
       </div>
     );
   }
