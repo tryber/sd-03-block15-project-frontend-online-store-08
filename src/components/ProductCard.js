3c23f9dd420e2171e6ae661cd2ff9d60deed3ebd
luldelrey <rpgstrikered@gmail.com>
2020-05-13 19:43:55 -0300

    some hot fixes

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,6 +7,7 @@ class ProductCard extends Component {
       <div data-testid="product">
         <h3>{product.title}</h3>
         <img src={product.thumbnail} alt="thumbnail" />
+        <p>{product.price}</p>
       </div>
     );
   }
