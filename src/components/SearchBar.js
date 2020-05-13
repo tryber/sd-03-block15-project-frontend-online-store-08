0c1358ff1ddeebf5b2f91a5eaff262d7805e8f90
luldelrey <rpgstrikered@gmail.com>
2020-05-13 17:57:12 -0300

    Search bar modificada para passar o termo pesquisado para uma callback

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,16 +12,25 @@ class SearchBar extends React.Component {
   }
 
   render() {
+    const { callback } = this.props;
     const { searchText } = this.state;
 
     return (
       <div className="App">
         <input
+          data-testid="query-input"
           type="text"
           placeholde="Digite algum termo de pesquisa ou escolha uma categoria."
           value={searchText}
           onChange={(event) => this.handleChange(event.target.value)}
         />
+        <button
+          data-test-id="query-button"
+          type="button"
+          onClick={() => callback(searchText)}
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
