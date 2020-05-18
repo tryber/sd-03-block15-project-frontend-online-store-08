a133dd87a2cec4133d8fa1aa500aef571a72f280
luldelrey <rpgstrikered@gmail.com>
2020-05-18 18:19:40 -0300

    reducer refatorado

--- src/components/Redux/reducer.js
+++ src/components/Redux/reducer.js
@@ -1,13 +1,13 @@
+let counter = 0;
+
 export default function reducer(state = { products: [], quantity: 0 }, action) {
   switch (action.type) {
     case 'addItem':
-      return {
-        ...state,
-        products: [...state.products, action.payload],
-        quantity: state.products.length,
-      };
+      counter += 1;
+      return { ...state, products: [...state.products, action.payload], quantity: counter };
 
     case 'removeItem':
+      counter -= 1;
       return state.products.filter((e) => e.product.id !== action.payload.id);
 
     default:
