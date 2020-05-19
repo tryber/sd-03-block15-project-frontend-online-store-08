63665aab6c97d990d7d5ddfa80d2a6bacb035af2
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-19 12:08:45 -0300

    codeclimate rev2

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import CheckoutList from './CheckoutList';
 import CheckoutTotal from './CheckoutTotal';
 import CheckoutForm from './CheckoutForm';
