bf0e5b59d6cedd303145cbe33595a2820c550508
luldelrey <rpgstrikered@gmail.com>
2020-05-22 10:38:54 -0300

    requisito 14!!

--- src/components/Redux/reducer.js
+++ src/components/Redux/reducer.js
@@ -1,43 +1,68 @@
-let counter = 0;
-
-export default function reducer(state = { products: [], quantity: 0 }, action) {
+const addItem = (state, action) => {
   const newProducts = state.products;
   const newProduct = action.payload;
   const hasItem = state.products.findIndex((a) => a.id === action.payload.id);
+
+  newProduct.quantity = 1;
+  if (hasItem === -1) {
+    return {
+      ...state,
+      products: [...state.products, newProduct],
+      quantity: state.products.reduce((acc, index) => acc + index.quantity, 1),
+    };
+  }
+
+  newProducts[hasItem].quantity += 1;
+  return {
+    ...state,
+    products: newProducts,
+    quantity: state.products.reduce((acc, index) => acc + index.quantity, 0),
+  };
+};
+
+const increment = (state, action) => {
+  const newProducts = state.products;
+  const newProduct = action.payload;
+  newProduct.quantity += 1;
+  if (newProduct.quantity < newProduct.available_quantity) {
+    newProduct.quantity += 1;
+    return {
+      ...state,
+      products: newProducts,
+      quantity: state.products.reduce((acc, index) => acc + index.quantity, 0),
+    };
+  }
+
+  return {
+    ...state,
+    products: newProducts,
+    quantity: state.products.reduce((acc, index) => acc + index.quantity, 0),
+  };
+};
+
+const decrement = (state, action) => {
+  const newProducts = state.products;
+  const newProduct = action.payload;
+  newProduct.quantity -= 1;
+
+  return {
+    ...state,
+    products: newProducts,
+    quantity: state.products.reduce((acc, index) => acc + index.quantity, 0),
+  };
+};
+
+export default function reducer(state = { products: [], quantity: 0 }, action) {
   switch (action.type) {
     case 'addItem':
-      counter += 1;
-      newProduct.quantity = 1;
-      if (hasItem === -1) {
-        return {
-          ...state,
-          products: [...state.products, newProduct],
-          quantity: counter,
-        };
-      }
-      newProducts[hasItem].quantity += 1;
-      return {
-        ...state,
-        products: newProducts,
-        quantity: counter,
-      };
+      return addItem(state, action);
+
     case 'incrementItem':
-      counter += 1;
-      newProduct.quantity += 1;
-      return {
-        ...state,
-        products: newProducts,
-        quantity: counter,
-      };
+      return increment(state, action);
 
     case 'decrementItem':
-      counter -= 1;
-      newProduct.quantity -= 1;
-      return {
-        ...state,
-        products: newProducts,
-        quantity: counter,
-      };
+      return decrement(state, action);
+
     default:
       return state;
   }
